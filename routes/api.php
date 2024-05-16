a5d8ec7f40244c6a50ec9f37c5d70a42f38c87fa
emillyluzia <emillyluzia416@gmail.com>
2024-05-16 15:53:37 -0300

    aaaaa

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\RoupasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('cadastroRoupas',[RoupasController::class,'cadastroRoupas']);
+Route::post('cadastroRoupas',[RoupasController::class,'cadastroRoupas']);
 Route::get('pesquisaCategoria',[RoupasController::class,'pesquisarPorCategoria']);
 Route::delete('excluir/{id}',[RoupasController::class,'excluir']);
 Route::get('vizualizar',[RoupasController::class,'retornarTodos']);
